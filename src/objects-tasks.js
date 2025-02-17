193b00cda3a5868a8ef11d7a7574e52fddcdc7e2
Constantine Saveliev <senior.saveliev2012@yandex.ru>
2025-02-17 17:15:57 +0300

    feat: add compareObjects

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -84,8 +84,17 @@ function removeProperties(obj, keys) {
  *    compareObjects({a: 1, b: 2}, {a: 1, b: 2}) => true
  *    compareObjects({a: 1, b: 2}, {a: 1, b: 3}) => false
  */
-function compareObjects(/* obj1, obj2 */) {
-  throw new Error('Not implemented');
+function compareObjects(obj1, obj2) {
+  const keys = Object.keys(obj1);
+  const keys2 = Object.keys(obj2);
+  if (keys.length !== keys2.length) return false;
+  for (let i = 0; i < keys.length; i += 1) {
+    const key = keys[i];
+    if (obj1[key] !== obj2[key]) {
+      return false;
+    }
+  }
+  return true;
 }
 
 /**
