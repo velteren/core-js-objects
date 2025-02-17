09134c86366a24e9ba8b76b074b52bcf5db4ef0c
Constantine Saveliev <senior.saveliev2012@yandex.ru>
2025-02-17 17:29:56 +0300

    feat: add makeWord

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -142,8 +142,17 @@ function makeImmutable(obj) {
  *    makeWord({ a: [0, 1], b: [2, 3], c: [4, 5] }) => 'aabbcc'
  *    makeWord({ H:[0], e: [1], l: [2, 3, 8], o: [4, 6], W:[5], r:[7], d:[9]}) => 'HelloWorld'
  */
-function makeWord(/* lettersObject */) {
-  throw new Error('Not implemented');
+function makeWord(lettersObject) {
+  const result = [];
+  const keys = Object.keys(lettersObject);
+  for (let i = 0; i < keys.length; i += 1) {
+    const key = keys[i];
+    const arr = lettersObject[key];
+    arr.forEach((item) => {
+      result[item] = key;
+    });
+  }
+  return result.join('');
 }
 
 /**
